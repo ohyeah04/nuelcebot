1fda0af2780b716dce6b0a9da368645d3c84eb99
ohyeah <davletiyarov@gmail.com>
2020-10-17 15:22:47 +0600

    exceptions handler on edit

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ async def on_ready():
     messageready = await channel.send('Now:')
 
     failedstatus = discord.Embed(title='Past Incidents:')
-    failedstatus.set_footer(text='\nEnd of Past Incidents Report'.format(datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)))
+    failedstatus.set_footer(text='\nEnd of Past Incidents Report'.format(
+        datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)))
     await failed.edit(embed=failedstatus)
 
     embed = discord.Embed(title='Web resources Live status')
@@ -38,12 +39,18 @@ async def on_ready():
     embed.add_field(name='Registrar', value='loading...', inline=True)
 
     embed.add_field(name='WebWork ', value='loading...', inline=True)
-    embed.add_field(name='Turnitin', value='loading...', inline=True)
+    embed.add_field(name='TurnitIn', value='loading...', inline=True)
     embed.add_field(name='Library', value='loading...', inline=True)
 
     embed.set_footer(text='\n✅ - Up\n❌ - Unavailable\nLast Update: not updated yet')
 
-    await messageready.edit(embed=embed)
+    try:
+        await messageready.edit(embed=embed)
+    except:
+        unknownstate = discord.Embed(title='Web resources Live status')
+        unknownstate.add_field(name='Failed to retrieve data', value='❌', inline=True)
+
+        await messageready.edit(embed=unknownstate)
 
     await query.start(messageready, failedstatus, failed)
 
@@ -51,13 +58,13 @@ async def on_ready():
 @tasks.loop(minutes=1.5, count=None)
 async def query(messageready, failedstatus, failed):
     moodle = '✅' if await checkstatus(moodleurl, failedstatus, failed) else '❌'
-    mynu = '✅' if await checkstatus(mynuurl,failedstatus, failed) else '❌'
-    lib = '✅' if await checkstatus(liburl,failedstatus, failed) else '❌'
-    webwork = '✅' if await checkstatus(webworkurl,failedstatus, failed) else '❌'
-    registrar = '✅' if await checkstatus(regisrarurl,failedstatus, failed) else '❌'
-    turnitin = '✅' if await checkstatus(turnitinurl,failedstatus, failed) else '❌'
+    mynu = '✅' if await checkstatus(mynuurl, failedstatus, failed) else '❌'
+    lib = '✅' if await checkstatus(liburl, failedstatus, failed) else '❌'
+    webwork = '✅' if await checkstatus(webworkurl, failedstatus, failed) else '❌'
+    registrar = '✅' if await checkstatus(regisrarurl, failedstatus, failed) else '❌'
+    turnitin = '✅' if await checkstatus(turnitinurl, failedstatus, failed) else '❌'
 
-    embed = discord.Embed(title='Web resources Live status')
+    embed = discord.Embed(title='Web resources Live status :check:')
     embed.add_field(name='Moodle', value=moodle, inline=True)
     embed.add_field(name='MyNU', value=mynu, inline=True)
     embed.add_field(name='Registrar', value=registrar, inline=True)
@@ -66,14 +73,21 @@ async def query(messageready, failedstatus, failed):
     embed.add_field(name='Turnitin', value=turnitin, inline=True)
     embed.add_field(name='Library', value=lib, inline=True)
 
-    embed.set_footer(text='\n✅ - Up\n❌ - Unavailable\nLast Update: {}'.format(datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)))
+    embed.set_footer(text='\n✅ - Up\n❌ - Unavailable\nLast Update: {}\nUpdates every minute'.format(
+        datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)))
 
-    await messageready.edit(embed=embed)
-
-
-async def checkstatus(url,failedstatus, failed):
     try:
-        requests.get(url, timeout=7)
+        await messageready.edit(embed=embed)
+    except:
+        unknownstate = discord.Embed(title='Web resources Live status')
+        unknownstate.add_field(name='Failed to retrieve data', value='❌', inline=True)
+
+        await messageready.edit(embed=unknownstate)
+
+
+async def checkstatus(url, failedstatus, failed):
+    try:
+        requests.get(url, timeout=5)
         return True
     except:
         await offline(url, failedstatus, failed)
@@ -97,8 +111,9 @@ async def offline(url, failedstatus, failed):
     elif url == turnitinurl:
         offlineresource = 'Turnitin'
 
-    failedstatus.insert_field_at(index=n, name=offlineresource, value='❌ at {}'.format(datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)), inline=False)
-    n = n+1
+    failedstatus.insert_field_at(index=n, name=offlineresource, value='❌ at {}'.format(
+        datetime.now(pytz.timezone('Asia/Almaty')).replace(microsecond=0, tzinfo=None)), inline=False)
+    n = n + 1
     await failed.edit(embed=failedstatus)
 
 
